24e0fe92774cb4013c3bb3305bbaaa49e20ea4e8
Ecaterina Pogorelova <Ecaterina.Pogorelova@endava.com>
2020-09-30 13:32:38 +0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./style.scss";
+
+class HomePage extends Component {
+  render() {
+    return <div className="home">work!</div>;
+  }
+}
+
+export default HomePage;
